6ad61529c23450e8f986279cfffa9cadf209aaed
KEFFA <muthurikeffa254@gmail.com>
2025-07-02 14:33:19 +0300

    About section implementation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import '@/components/home.css';
+import { MoveUpRight } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -51,6 +52,25 @@ export default function Home() {
       <div className="about" id="about">
         <div className="card_cont">
           <div className="about_card">
+            <section className="content_card">
+              <div className="content">
+                The Responsible Computing Challenge aims to embed ethical computing practices into academic
+                curricula and professional practices. Faculty and students are being trained to navigate
+                ethical concerns such as data privacy, cybersecurity, and algorithmic bias.
+              </div>
+              {/* Through workshops, academic programs, and industry collaborations, the University is shaping
+              a new generation of tech professionals who are not only skilled in technology but also aware
+              of its societal and environmental implications. */}
+                <button className='content_more'>View More
+                  <MoveUpRight size={18}/>
+                </button>
+            </section>
+            <section className="workshop">
+              <div className="workshop_ttle">
+                Workshop
+              </div>
+            </section>
+
             <section className="img">
               <Image
                 src="/images/about.jpeg"
@@ -58,17 +78,6 @@ export default function Home() {
                 width={1200}
                 height={600}
               />
-            </section>
-
-            <section className="content">
-              Kisii University community led by The Deputy Vice Chancellor Prof. F. Mzee Awour partnering
-              with the Mozilla Foundation and USAID Kenya for a transformative project worth $62,500.
-              The Responsible Computing Challenge aims to embed ethical computing practices into academic
-              curricula and professional practices. Faculty and students are being trained to navigate
-              ethical concerns such as data privacy, cybersecurity, and algorithmic bias.
-              Through workshops, academic programs, and industry collaborations, the University is shaping
-              a new generation of tech professionals who are not only skilled in technology but also aware
-              of its societal and environmental implications.
             </section>
           </div>
         </div>
