c5305e24589b991475f7cb35f6f43010801a258f
KEFFA <muthurikeffa254@gmail.com>
2025-07-28 15:54:17 +0300

    import link update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import '@/app/hero/register/page.css';
+import '@/app/register/page.css';
 import '@/app/globals.css';
 
 export default function register() {
