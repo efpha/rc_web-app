816d6f99ebad35c7dc8ad1ad6499ecb372936593
KEFFA <muthurikeffa254@gmail.com>
2025-07-28 15:50:56 +0300

    register page

---
+++ src/app/hero/register/page.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import React, { useState } from 'react';
+import '@/app/hero/register/page.css';
+import '@/app/globals.css';
+
+export default function register() {
+  const [notify, setNotify] = useState(false);
+    return (
+    <div className="signup-container">
+      <p className="signup-intro">
+        Get notified about upcoming events and workshops by registering below.
+      </p>
+      <h2 className="signup-title">Sign Up</h2>
+      <form className="signup-form">
+        <div className="form-group">
+          <label htmlFor="firstName">First Name<span className='star'>*</span></label>
+          <input
+            type="text"
+            id="firstName"
+            placeholder="John"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name <span className='star'>*</span></label>
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Doe"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Registration Number <span className='star'>*</span></label>
+          <input
+            type="text"
+            id="registrationNumber"
+            placeholder="e.g. inxx/xxxx/xx"
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email <span className='star'>*</span></label>
+          <input
+            type="email"
+            id="email"
+            placeholder="e.g. johndoe@example.com"
+            required
+          />
+        </div>
+
+        <div className="form-group checkbox-group">
+          <input
+            type="checkbox"
+            id="notify"
+            checked={notify}
+            onChange={(e) => setNotify(e.target.checked)}
+          />
+          <label htmlFor="notify" className="checkbox-label">
+            Receive notification on up-coming events
+          </label>
+        </div>
+
+        <button type="submit" className="register-button">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
