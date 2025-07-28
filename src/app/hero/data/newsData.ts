729119c1683fe922a69ae8b2f69b7029eaea9cc0
KEFFA <muthurikeffa254@gmail.com>
2025-07-28 09:39:17 +0300

    directory updated to be under hero

---
+++ src/app/hero/data/newsData.ts
@@ -0,0 +1,38 @@
+// Add cards dynamically to pages
+export const newsItems = [
+    {
+      title: 'Vice Chancellor receive and award the winning projects from the Responsible Computing Innovation Day',
+      tag: 'Project Award',
+      image: '/images/abs9pxPPxlRK8NsOsKsSGcTZWBuRg8UoYLPd2jG5.jpg',
+      link: 'https://kisiiuniversity.ac.ke/blog/responsible-computing-innovation-day-awards',
+      date: 'Apr 24th, 2025'
+    },
+    {
+      title: 'Students from the School of Information Science & Technology present their innovations',
+      tag: "Innovation",
+      image: '/images/QqD8ha1koXoJQES9sCBN6ZE5h6h49I4ehpUyNl1c.jpg',
+      link: 'https://kisiiuniversity.ac.ke/blog/mozilla-sponsored-responsible-computing-innovation-day',
+      date: 'Apr 24th, 2025'
+    },
+    {
+      title: 'Students from School of Information Science & Technology overwhelmingly embraced tutelage in matters ethics',
+      tag: "Tutelage",
+      image: '/images/OYYviaxMG7P0lNXUCHGSe4hlvrkIHHNZeg0AkMVc.jpg',
+      link: 'https://kisiiuniversity.ac.ke/blog/mozilla-foundation-responsible-computing-challenge',
+      date: 'Jan 29th, 2025'
+    },
+    {
+      title: 'Faculty team from School of Information Science and technology set the ball rolling',  
+      tag: 'Project Kickoff',
+      image: '/images/547Nr9x1BkS7amVE7wMDg2i1KNG2sdhq1D7M2G2O.jpg',
+      link: '/workshops/second-phase-begin',
+      date: 'Jan 28th, 2025'
+    },
+    {
+      title: 'Pre-Project Workshop',
+      tag: 'Research',
+      image: '/images/Bzw1uag6HA1zAFz5eCpy8CggmDOweknQNT7rAdRk.jpg',
+      link: '/workshops/workshop1',
+      date: 'Jan 27th, 2025'
+    }
+];
