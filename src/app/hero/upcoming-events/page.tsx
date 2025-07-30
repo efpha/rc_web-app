e2f46fec4aa63d39e94b85a58bc3d7560f3f4ea2
KEFFA <muthurikeffa254@gmail.com>
2025-07-30 16:09:24 +0300

    Implementation of no event scheduled

--- src/app/hero/upcoming-events/page.tsx
+++ src/app/hero/upcoming-events/page.tsx
@@ -5,6 +5,9 @@ import '@/app/hero/upcoming-events/page.css';
 import { ArrowUpRight } from 'lucide-react';
 import Image from 'next/image';
 import Footer from '@/components/footer';
+// import { div } from 'three/tsl';
+import Link from 'next/link';
+import Register from '@/app/register/page';
 
 type Event = {
   id: number;
@@ -17,38 +20,38 @@ type Event = {
 
 export default function Events() {
   const [events] = useState<Event[]>([
-    {
-      id: 1,
-      category: 'Workshop',
-      title: 'Responsible Computing: Ethical Tech in Practice',
-      image: '/images/fotis-fotopoulos-6sAl6aQ4OWI.jpg',
-      date: 'Aug 10',
-      time: '10:00 AM',
-    },
-    {
-      id: 3,
-      category: 'Seminar',
-      title: 'AI & Bias: A Responsible Computing Perspective',
-      image: '/images/0iV9LmPDn0.jpg',
-      date: 'Aug 18',
-      time: '2:00 PM',
-    },
-    {
-      id: 4,
-      category: 'Panel Discussion',
-      title: 'Tech for Good: Sustainability & Responsible Innovation',
-      image: '/images/CosHjyONRk8.jpg',
-      date: 'Aug 25',
-      time: '4:30 PM',
-    },
-    {
-      id: 5,
-      category: 'Webinar',
-      title: 'Privacy by Design: A Responsible Computing Approach',
-      image: '/images/FnA5pAzqhMM.jpg',
-      date: 'Sep 3',
-      time: '11:00 AM',
-    },
+    // {
+    //   id: 1,
+    //   category: 'Workshop',
+    //   title: 'Responsible Computing: Ethical Tech in Practice',
+    //   image: '/images/fotis-fotopoulos-6sAl6aQ4OWI.jpg',
+    //   date: 'Aug 10',
+    //   time: '10:00 AM',
+    // }
+    // {
+    //   id: 3,
+    //   category: 'Seminar',
+    //   title: 'AI & Bias: A Responsible Computing Perspective',
+    //   image: '/images/0iV9LmPDn0.jpg',
+    //   date: 'Aug 18',
+    //   time: '2:00 PM',
+    // },
+    // {
+    //   id: 4,
+    //   category: 'Panel Discussion',
+    //   title: 'Tech for Good: Sustainability & Responsible Innovation',
+    //   image: '/images/CosHjyONRk8.jpg',
+    //   date: 'Aug 25',
+    //   time: '4:30 PM',
+    // },
+    // {
+    //   id: 5,
+    //   category: 'Webinar',
+    //   title: 'Privacy by Design: A Responsible Computing Approach',
+    //   image: '/images/FnA5pAzqhMM.jpg',
+    //   date: 'Sep 3',
+    //   time: '11:00 AM',
+    // },
   ]);
 
   return (
@@ -58,10 +61,13 @@ export default function Events() {
         <h2>Upcoming events</h2>
       </div>
       <div className='underline'></div>
-
       <section className='card-section'>
         {events.length === 0 ? (
-          <div className='noEvents'>No upcoming events available.</div>
+          <>
+          <h2 className="noEvents">No event scheduled. Subscribe to get notified when there's one.</h2>
+          <Register />
+          </>
+          
         ) : (
           <div className='grid'>
             {events.map((event) => (
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -133,7 +133,6 @@ export default function Register() {
           </button>
         </form>
       </div>
-      <Footer/>
     </>
   );
 }
