65bbda2ea91684272418fc9c133124f010087a43
KEFFA <muthurikeffa254@gmail.com>
2025-07-31 20:18:49 +0300

    Change of tittle name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Responsible computing",
+  title: "Mozilla Responsible Computing Challenge",
   description: "",
 };
 
