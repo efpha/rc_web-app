16867d22d8a2549ddd42f4b8de3954ce1f0aa0c1
KEFFA <muthurikeffa254@gmail.com>
2025-08-07 20:21:57 +0300

    New Images

--- src/app/hero/data/newsData.ts
+++ src/app/hero/data/newsData.ts
@@ -1,118 +1,179 @@
-
 // Add cards dynamically to pages
 export const newsItems = [
-  //START ADDING FROM HERE NOT FROM THE END
-    {
-      title: 'Discussion with panel drawn from all over the globe on Mozilla Responsible Computing Project.',
-      tag: 'Industry Workshop',
-      image: '/images/GmTJ1JRb0AERhxU.jpg',
-      link: 'https://kisiiuniversity.ac.ke/blog/responsible-computing-innovation-day-awards',
-      date: 'Mar 18th, 2025'
-    },
-    {
-      title: 'Vice Chancellor receive and award the winning projects from the Responsible Computing Innovation Day',
-      tag: 'Project Award',
-      image: '/images/abs9pxPPxlRK8NsOsKsSGcTZWBuRg8UoYLPd2jG5.jpg',
-      link: 'https://kisiiuniversity.ac.ke/blog/responsible-computing-innovation-day-awards',
-      date: 'Apr 24th, 2025'
-    },
-    {
-      title: 'Students from the School of Information Science & Technology present innovations',
-      tag: "Project Exhibition",
-      image: '/images/QqD8ha1koXoJQES9sCBN6ZE5h6h49I4ehpUyNl1c.jpg',
-      link: 'https://kisiiuniversity.ac.ke/blog/mozilla-sponsored-responsible-computing-innovation-day',
-      date: 'Apr 24th, 2025'
-    },
-    {
-      title: 'Students from School of Information Science & Technology embrace tutelage in matters ethics overwhelmingly',
-      tag: "Students Tutelage",
-      image: '/images/OYYviaxMG7P0lNXUCHGSe4hlvrkIHHNZeg0AkMVc.jpg',
-      link: 'https://kisiiuniversity.ac.ke/blog/mozilla-foundation-responsible-computing-challenge',
-      date: 'Jan 29th, 2025'
-    },
-    {
-      title: 'Faculty team from School of Information Science and technology set the ball rolling for phase 2 of Mozilla Responsible computing',  
-      tag: 'Faculty Meet',
-      image: '/images/GiEX5o5XkAA8U4y.jpg',
-      link: 'https://kisiiuniversity.ac.ke/blog/mozilla-responsible-computing-challenge-2nd-phase-begins',
-      date: 'Jan 28th, 2025'
-    },
-    {
-      title: 'The School of Information Science and Technology staff take a proactive role in embedding Responsible Computing practices into the curriculum of Computer Science courses',  
-      tag: 'Faculty Meet',
-      image: '/images/GJnghKhXEAA__zV.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1772694797548949689',
-      date: 'Mar 26th, 2024'
-    },
-    {
-      title: 'Dr. Chao Mbogho, Kenya Senior Fellow of the Mozilla Responsible Computing Challenge engage with groups operating the RCC project in the University',
-      tag: 'Senior Fellow Visit',
-      image: '/images/GH6_N84W0AAJrDc.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1765058357075779994',
-      date: 'Mar 5th, 2024'
-    },
-    {
-      title: 'Computing Sciences faculty carry out a workshop to expound on how best to integrate Responsible Computing in Computing Science courses.',  
-      tag: 'Discourse Enlightenment',
-      image: '/images/GGhWWKoW4AACrcz.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1758750598004887776',
-      date: 'Feb 17th, 2024'
-    },
-    {
-      title: 'Students delve down into responsible computing workshop from Ethical AI outlook.',  
-      tag: 'Responsible Computing Exploration',
-      image: '/images/F9LfeZPXsAAxRRp.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1758360722554187920',
-      date: 'Feb 16th, 2024'
-    },
-    {
-      title: 'Computing Science faculty interrogation on how to deliver Responsible Computing programs.',  
-      tag: 'Faculty Meet',
-      image: '/images/GGMURL0WIAEkmB8.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1757270568796090679',
-      date: 'Feb 13th, 2024'
-    },
-    {
-      title: 'Students presented their innovations based on knowledge gained from Ethical AI. Mental AI chatbot carried the day.',  
-      tag: 'Ethical AI Exhibition',
-      image: '/images/F_EBJtfWgAAw73Z.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1725161419929223529',
-      date: 'Nov 16th, 2023'
-    },
-    {
-      title: 'Students perfect the art and science of Ethical Artificial intelligence after rigorous engagement with experts.',  
-      tag: 'Experts vs Students Engagement',
-      image: '/images/F9Lfe8UWcAAgHkw.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1716680133661032559',
-      date: 'Oct 24th, 2023'
-    },
-    {
-      title: 'Students engage with Ethical AI experts to learn how best to be ethical.',  
-      tag: 'Experts vs Students Engagement',
-      image: '/images/F64Rhh_XYAAvWJ6.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1706320606872900089',
-      date: 'Sep 25th, 2023'
-    },
-    {
-      title: 'Ethical Artificial Intelligence paper being presented at Kisii University Business Conference.',  
-      tag: 'Ethical AI Business Conference',
-      image: '/images/F6cUo73XAAAMm-g.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1704353685612953716',
-      date: 'Sep 20th, 2023'
-    },
-    {
-      title: 'The School of Information Science and Technology students induced into Ethical AI project for the benefit of their career and society at large.',  
-      tag: 'Student Induction',
-      image: '/images/F6XLMWsWUAA01Iv.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1703991462197633419',
-      date: 'Sep 19th, 2023'
-    },
-    {
-      title: 'Kisii University and Mozilla commence a partnership project aimed to see IT students to enmesh ethics in their studies.',  
-      tag: 'Project Commencement',
-      image: '/images/F4G-4HFXEAAgCAh.jpg',
-      link: 'https://x.com/kisiuniofficial/status/1693844810220667092',
-      date: 'Aug 22th, 2023'
-    }
+  // start adding from here
+  {
+    title: 'Discussion with panel drawn from all over the globe on Mozilla Responsible Computing Project.',
+    tag: 'Industry Workshop',
+    image: '/images/GmTJ1JRb0AERhxU.jpg',
+    pictures: [
+      '/images/GmTJ1JRb0AERhxU.jpg'
+    ],
+    link: 'https://kisiiuniversity.ac.ke/blog/responsible-computing-innovation-day-awards',
+    date: 'Mar 18th, 2025'
+  },
+  {
+    title: 'Vice Chancellor receive and award the winning projects from the Responsible Computing Innovation Day',
+    tag: 'Project Award',
+    image: '/images/abs9pxPPxlRK8NsOsKsSGcTZWBuRg8UoYLPd2jG5.jpg',
+    pictures: [
+      '/images/abs9pxPPxlRK8NsOsKsSGcTZWBuRg8UoYLPd2jG5.jpg'
+    ],
+    link: 'https://kisiiuniversity.ac.ke/blog/responsible-computing-innovation-day-awards',
+    date: 'Apr 24th, 2025'
+  },
+  {
+    title: 'Students from the School of Information Science & Technology present innovations',
+    tag: 'Project Exhibition',
+    image: '/images/QqD8ha1koXoJQES9sCBN6ZE5h6h49I4ehpUyNl1c.jpg',
+    pictures: [
+      '/images/QqD8ha1koXoJQES9sCBN6ZE5h6h49I4ehpUyNl1c.jpg'
+    ],
+    link: 'https://kisiiuniversity.ac.ke/blog/mozilla-sponsored-responsible-computing-innovation-day',
+    date: 'Apr 24th, 2025'
+  },
+  {
+    title: 'Students from School of Information Science & Technology embrace tutelage in matters ethics overwhelmingly',
+    tag: 'Students Tutelage',
+    image: '/images/OYYviaxMG7P0lNXUCHGSe4hlvrkIHHNZeg0AkMVc.jpg',
+    pictures: [
+      '/images/OYYviaxMG7P0lNXUCHGSe4hlvrkIHHNZeg0AkMVc.jpg'
+    ],
+    link: 'https://kisiiuniversity.ac.ke/blog/mozilla-foundation-responsible-computing-challenge',
+    date: 'Jan 29th, 2025'
+  },
+  {
+    title: 'Faculty team from School of Information Science and technology set the ball rolling for phase 2 of Mozilla Responsible computing',  
+    tag: 'Faculty Meet',
+    image: '/images/GiEX5o5XkAA8U4y.jpg',
+    pictures: [
+      '/images/547Nr9x1BkS7amVE7wMDg2i1KNG2sdhq1D7M2G2O.jpg',
+      '/images/679942018929d.jpeg',
+      '/images/6799420192c3e.jpeg',
+      '/images/679942019b8b2.jpeg',
+      '/images/67994201a3d1b.jpeg',
+      '/images/67994201abe96.jpeg',
+      '/images/67994201b454d.jpeg',
+      '/images/67994201bcb4a.jpeg',
+      '/images/67994201c4b52.jpeg'
+    ],
+    link: 'https://kisiiuniversity.ac.ke/blog/mozilla-responsible-computing-challenge-2nd-phase-begins',
+    date: 'Jan 28th, 2025'
+  },
+  {
+    title: 'The School of Information Science and Technology staff take a proactive role in embedding Responsible Computing practices into the curriculum of Computer Science courses',  
+    tag: 'Faculty Meet',
+    image: '/images/GJnghKhXEAA__zV.jpg',
+    pictures: [
+      '/images/GJnghJfXcAAuKNt.jpg',
+      '/images/GJnghJrWEAAqNdG.jpg',
+      '/images/GJnghK4XIAAQQEy.jpg',
 
+    ],
+    link: 'https://x.com/kisiuniofficial/status/1772694797548949689',
+    date: 'Mar 26th, 2024'
+  },
+  {
+    title: 'Dr. Chao Mbogho, Kenya Senior Fellow of the Mozilla Responsible Computing Challenge engage with groups operating the RCC project in the University',
+    tag: 'Senior Fellow Visit',
+    image: '/images/GH6_N84W0AAJrDc.jpg',
+    pictures: [
+      '/images/GH6_N-zXsAAaiGQ.jpg',
+      '/images/GH6_N_EXIAAAhh1.jpg',
+      '/images/GH6_N-JWQAA_YjK.jpg'
+    ],
+    link: 'https://x.com/kisiuniofficial/status/1765058357075779994',
+    date: 'Mar 5th, 2024'
+  },
+  {
+    title: 'Computing Sciences faculty carry out a workshop to expound on how best to integrate Responsible Computing in Computing Science courses.',  
+    tag: 'Discourse Enlightenment',
+    image: '/images/GGhWWKoW4AACrcz.jpg',
+    pictures: [
+      '/images/GGhWWJUWQAA5C3S.jpg',
+      '/images/GGhWWMtWQAA0jBS.jpg',
+      '/images/GGhWWKYWUAAVHBh.jpg'
+    ],
+    link: 'https://x.com/kisiuniofficial/status/1758750598004887776',
+    date: 'Feb 17th, 2024'
+  },
+  {
+    title: 'Students delve down into responsible computing workshop from Ethical AI outlook.',  
+    tag: 'Responsible Computing Exploration',
+    image: '/images/F9LfeZPXsAAxRRp.jpg',
+    link: 'https://x.com/kisiuniofficial/status/1758360722554187920',
+    date: 'Feb 16th, 2024'
+  },
+  {
+    title: 'Computing Science faculty interrogation on how to deliver Responsible Computing programs.',  
+    tag: 'Faculty Meet',
+    image: '/images/GGMURL0WIAEkmB8.jpg',
+    link: 'https://x.com/kisiuniofficial/status/1757270568796090679',
+    date: 'Feb 13th, 2024'
+  },
+  {
+    title: 'Students presented their innovations based on knowledge gained from Ethical AI. Mental AI chatbot carried the day.',  
+    tag: 'Ethical AI Exhibition',
+    image: '/images/F_EBJtfWgAAw73Z.jpg',
+    pictures: [
+      '/images/F_EBKDsWUAANxGr.jpg',
+      '/images/F_EBKYJWwAEb7-D.jpg',
+      '/images/F_EBKu8WIAAaA7L.jpg',
+
+    ],
+    link: 'https://x.com/kisiuniofficial/status/1725161419929223529',
+    date: 'Nov 16th, 2023'
+  },
+  {
+    title: 'Students perfect the art and science of Ethical Artificial intelligence after rigorous engagement with experts.',  
+    tag: 'Experts vs Students Engagement',
+    image: '/images/F9Lfe8UWcAAgHkw.jpg',
+    link: 'https://x.com/kisiuniofficial/status/1716680133661032559',
+    date: 'Oct 24th, 2023'
+  },
+  {
+    title: 'Students engage with Ethical AI experts to learn how best to be ethical.',  
+    tag: 'Experts vs Students Engagement',
+    image: '/images/F64Rhh_XYAAvWJ6.jpg',
+    pictures: [
+      '/images/F64Rh_2WwAAyEAW.jpg',
+      '/images/F64RiK-WYAA_NdJ.jpg',
+      '/images/F64Rir1XQAADbzp.jpg',
+      '/images/F9Lfd_tWIAAcP7s.jpg',
+      '/images/F9LfeZPXsAAxRRp-fuse.jpg',
+      '/images/F9Lfer-WAAAB3T6.jpg'
+    ],
+    link: 'https://x.com/kisiuniofficial/status/1706320606872900089',
+    date: 'Sep 25th, 2023'
+  },
+  {
+    title: 'Ethical Artificial Intelligence paper being presented at Kisii University Business Conference.',  
+    tag: 'Ethical AI Business Conference',
+    image: '/images/F6cUoXWXYAALRWK.jpg',
+    pictures: [
+      '/images/F6cUo73XAAAMm-g.jpg',
+      '/images/F6cUonHWMAAj0gA.jpg',
+      '/images/F6cUoC6WsAAg6kH.jpg'
+    ],
+    link: 'https://x.com/kisiuniofficial/status/1704353685612953716',
+    date: 'Sep 20th, 2023'
+  },
+  {
+    title: 'The School of Information Science and Technology students induced into Ethical AI project for the benefit of their career and society at large.',  
+    tag: 'Student Induction',
+    image: '/images/F6XLMWsWUAA01Iv.jpg',
+    pictures: [
+      '/images/F6XLL41XcAAs_qJ.jpg',
+      '/images/F6XLMvjXAAAHjUO.jpg',
+      '/images/F6XLNBUXMAAa-t1.jpg'
+      ],
+    link: 'https://x.com/kisiuniofficial/status/1703991462197633419',
+    date: 'Sep 19th, 2023'
+  },
+  {
+    title: 'Kisii University and Mozilla commence a partnership project aimed to see IT students to enmesh ethics in their studies.',  
+    tag: 'Project Commencement',
+    image: '/images/F4G-4HFXEAAgCAh.jpg',
+    link: 'https://x.com/kisiuniofficial/status/1693844810220667092',
+    date: 'Aug 22th, 2023'
+  }
 ];
