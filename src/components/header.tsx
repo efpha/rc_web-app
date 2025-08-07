a0af7a9fffa22ec6d4b2671530b5435c80705aea
KEFFA <muthurikeffa254@gmail.com>
2025-08-07 10:13:07 +0300

    Additional of top nav bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { useState } from 'react';
 import { Logs, X, MoveUpRight} from 'lucide-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook,faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import Link from 'next/link';
 import '@/components/header.css';
 import '@/app/hero/about/page'
@@ -12,54 +14,67 @@ export default function Header() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <header className="nav-bar">
-      <div className="lgo-txt">
-        <Link href="/">
-          <div>RC Challenge</div>
-        </Link>
-      </div>
-
-      {/* Hamburger icon for mobile */}
-      <button
-        className="menu-icon"
-        onClick={() => setSidebarOpen(true)}
-        aria-label="Open menu"
-      >
-        <Logs />
-      </button>
-
-      {/* Desktop links */}
-      <nav className="links">
-        <Link href="/hero">Home</Link>
-        <Link href="/hero/about">About</Link>
-        <Link href="/hero/insights">Insights</Link>
-        <Link href="/hero/workshops">Workshops</Link>
-        <Link href="/hero/upcoming-events">Upcoming Events</Link>
-      </nav>
-
-      {/* Sidebar overlay for mobile */}
-      {sidebarOpen && (
-        <div className="sidebar-overlay" onClick={() => setSidebarOpen(false)}>
-          <aside className="sidebar" onClick={e => e.stopPropagation()}>
-            <section className='side-wrapper'>
-                <button
-                  className="close-btn"
-                  onClick={() => setSidebarOpen(false)}
-                  aria-label="Close menu"
-                >
-                  <X />
-                </button>
-                <nav>
-                  <Link href="/hero" className='link' onClick={() => setSidebarOpen(false)}>Home<MoveUpRight size={16}/></Link>
-                  <Link href="/hero/about" className='link' onClick={() => setSidebarOpen(false)}>About <MoveUpRight size={16}/></Link>
-                  <Link href="/hero/insights" className='link' onClick={() => setSidebarOpen(false)}>Insights <MoveUpRight size={16}/></Link>
-                  <Link href="/hero/workshops" className='link' onClick={() => setSidebarOpen(false)}>Workshops <MoveUpRight size={16}/></Link>
-                  <Link href="/hero/upcoming-events" className='link' onClick={() => setSidebarOpen(false)}>Upcoming Events <MoveUpRight size={16}/></Link>
-                </nav>
-            </section>
-          </aside>
+    <>
+      <header className='top-navbar'>
+        <div className="socials-media">
+          <Link href=''><FontAwesomeIcon icon={faFacebook} size="2x" color="#1f456e" /></Link>
+          <Link href=''><FontAwesomeIcon icon={faXTwitter} size="2x" color="#1f456e" /></Link>
+          <Link href=''><FontAwesomeIcon icon={faLinkedin} size="2x" color="#1f456e" /></Link>
         </div>
-      )}
-    </header>
+        <div className="ksu">
+          <Link href='https://kisiiuniversity.ac.ke/'>KISII UNIVERSITY</Link>
+        </div>
+      </header>
+        <header className="nav-bar">
+          <div className="lgo-txt">
+            <Link href="/">
+              <div>RC Challenge</div>
+            </Link>
+          </div>
+
+          {/* Hamburger icon for mobile */}
+          <button
+            className="menu-icon"
+            onClick={() => setSidebarOpen(true)}
+            aria-label="Open menu"
+          >
+            <Logs />
+          </button>
+
+          {/* Desktop links */}
+          <nav className="links">
+            <Link href="/hero">Home</Link>
+            <Link href="/hero/about">About</Link>
+            <Link href="/hero/insights">Insights</Link>
+            <Link href="/hero/workshops">Workshops</Link>
+            <Link href="/hero/upcoming-events">Upcoming Events</Link>
+          </nav>
+
+          {/* Sidebar overlay for mobile */}
+          {sidebarOpen && (
+            <div className="sidebar-overlay" onClick={() => setSidebarOpen(false)}>
+              <aside className="sidebar" onClick={e => e.stopPropagation()}>
+                <section className='side-wrapper'>
+                    <button
+                      className="close-btn"
+                      onClick={() => setSidebarOpen(false)}
+                      aria-label="Close menu"
+                    >
+                      <X />
+                    </button>
+                    <nav>
+                      <Link href="/hero" className='link' onClick={() => setSidebarOpen(false)}>Home<MoveUpRight size={16}/></Link>
+                      <Link href="/hero/about" className='link' onClick={() => setSidebarOpen(false)}>About <MoveUpRight size={16}/></Link>
+                      <Link href="/hero/insights" className='link' onClick={() => setSidebarOpen(false)}>Insights <MoveUpRight size={16}/></Link>
+                      <Link href="/hero/workshops" className='link' onClick={() => setSidebarOpen(false)}>Workshops <MoveUpRight size={16}/></Link>
+                      <Link href="/hero/upcoming-events" className='link' onClick={() => setSidebarOpen(false)}>Upcoming Events <MoveUpRight size={16}/></Link>
+                    </nav>
+                </section>
+              </aside>
+            </div>
+          )}
+      </header>
+    </>
+
   );
 }
