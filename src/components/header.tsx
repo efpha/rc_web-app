bbad9c4b6986dcce4b3fe9ef35f0dbfe1f8a2ee9
KEFFA <muthurikeffa254@gmail.com>
2025-08-07 11:53:06 +0300

    Top nav bar with logos

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,11 @@
 import { useState } from 'react';
 import { Logs, X, MoveUpRight} from 'lucide-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook,faXTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook,faXTwitter, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 import Link from 'next/link';
+import Image from 'next/image';
+
 import '@/components/header.css';
 import '@/app/hero/about/page'
 import '@/app/hero/workshops/page'
@@ -16,19 +19,39 @@ export default function Header() {
   return (
     <>
       <header className='top-navbar'>
-        <div className="socials-media">
-          <Link href=''><FontAwesomeIcon icon={faFacebook} size="2x" color="#1f456e" /></Link>
-          <Link href=''><FontAwesomeIcon icon={faXTwitter} size="2x" color="#1f456e" /></Link>
-          <Link href=''><FontAwesomeIcon icon={faLinkedin} size="2x" color="#1f456e" /></Link>
+        <div className="top-navbar-container">
+
+          {/*Social media icons*/}
+          <div className="socials-media">
+            <Link href='https://www.facebook.com/Kisiiuniversityofficial/'><FontAwesomeIcon icon={faFacebook} size="2x" color="#1f456e" /></Link>
+            <Link href='https://x.com/kisiuniofficial'><FontAwesomeIcon icon={faXTwitter} size="2x" color="#1f456e" /></Link>
+            <Link href='https://www.linkedin.com/company/kisiiuniversityofficial'><FontAwesomeIcon icon={faLinkedin} size="2x" color="#1f456e" /></Link>
+            <Link href='https://www.instagram.com/kisiiuniversityofficial/'><FontAwesomeIcon icon={faInstagram} size="2x" color="#1f456e" /></Link>
+          </div>
+
+          {/*University name and logo*/}
+          <div className="ksu">
+            <Link href='https://kisiiuniversity.ac.ke/'>
+              <p>Kisii University</p>
+                <div className="ksu-logo">
+                  <Image
+                      src="/images/ksu.png"
+                      alt="Kisii University Logo"
+                      width={160}
+                      height={160}
+                    />
+                </div>
+            </Link>          
+          </div>
+
+          
         </div>
-        <div className="ksu">
-          <Link href='https://kisiiuniversity.ac.ke/'>KISII UNIVERSITY</Link>
-        </div>
+        
       </header>
         <header className="nav-bar">
           <div className="lgo-txt">
             <Link href="/">
-              <div>RC Challenge</div>
+              <div>Responsible Computing Challenge</div>
             </Link>
           </div>
 
@@ -48,6 +71,7 @@ export default function Header() {
             <Link href="/hero/insights">Insights</Link>
             <Link href="/hero/workshops">Workshops</Link>
             <Link href="/hero/upcoming-events">Upcoming Events</Link>
+            <Link href="/hero/upcoming-events">Media</Link>
           </nav>
 
           {/* Sidebar overlay for mobile */}
@@ -68,6 +92,8 @@ export default function Header() {
                       <Link href="/hero/insights" className='link' onClick={() => setSidebarOpen(false)}>Insights <MoveUpRight size={16}/></Link>
                       <Link href="/hero/workshops" className='link' onClick={() => setSidebarOpen(false)}>Workshops <MoveUpRight size={16}/></Link>
                       <Link href="/hero/upcoming-events" className='link' onClick={() => setSidebarOpen(false)}>Upcoming Events <MoveUpRight size={16}/></Link>
+                      <Link href="/hero/upcoming-events" className='link' onClick={() => setSidebarOpen(false)}>Media <MoveUpRight size={16}/></Link>
+
                     </nav>
                 </section>
               </aside>
